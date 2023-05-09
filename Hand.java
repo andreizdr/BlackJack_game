b3d29193fb813262635055a961c159792fc62f87
andreizdr <zdravcov.andrei@gmail.com>
2023-05-09 19:31:22 +0300

    Changed the getPoints() algorithm. Now shows correct points in each hand.

--- Hand.java
+++ Hand.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Hand {
     private ArrayList<Card> cards = new ArrayList<>();
     Deck deck;
-
+    int tempPoints = 0;
     int points = 0;
     public Hand(Deck deck) {
         this.deck = deck;
@@ -26,9 +26,11 @@ public class Hand {
     public int getPoints() {            // Calculate points of each hand
         for (int i = 0; i < cards.size(); i++) {
             if (cards.get(i).isFaceDown == false) {
-                points += cards.get(i).getRank();
+                tempPoints += cards.get(i).getRank();
             }
         }
+        points = tempPoints;
+        tempPoints = 0;
         return points;
     }
 
