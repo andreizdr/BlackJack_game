fc90a5e155c9706adeaa3394878ea29689e8a6f1
andreizdr <zdravcov.andrei@gmail.com>
2023-05-10 18:02:25 +0300

    Added setAceValue() to switch between Ace values.

--- Rank.java
+++ Rank.java
@@ -1,10 +1,10 @@
 public enum Rank {
-    ACE(1,"Ace"), TWO(2,"Two"), THREE(3,"Three"), FOUR(4,"Four"),
+    ACE(11,"Ace"), TWO(2,"Two"), THREE(3,"Three"), FOUR(4,"Four"),
     FIVE(5,"Five"), SIX(6,"Six"), SEVEN(7,"Seven"), EIGHT(8,"Eight"),
     NINE(9,"Nine"), TEN(10,"Ten"), JACK(10,"Jack"),
     QUEEN(10,"Queen"), KING(10,"King");
 
-    private final int value;
+    private  int value;
     private final String name;
 
     Rank(int value, String name) {
@@ -19,5 +19,9 @@ public enum Rank {
 
     public String getRankName() {
         return name;
+    }
+
+    public void setAceValue() {
+        this.value = 1;
     }
 }
